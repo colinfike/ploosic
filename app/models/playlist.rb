cb88e36a3fd359f62e0949eb068304a0634f837d
Colin Fike <colin.fike@gmail.com>
2017-03-05 00:08:37 -0500

    Playlist tests are green

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,2 +1,7 @@
 class Playlist < ApplicationRecord
+  belongs_to :user
+  has_many :track_playlists
+  has_many :tracks, through: :track_playlists
+
+  validate :user, :presence
 end
--- test/models/playlist_test.rb
+++ test/models/playlist_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class PlaylistTest < ActiveSupport::TestCase
   test "playlist can have many tracks" do
-    playlist = playlists(:has_tracks)
+    playlist = playlists(:valid_playlist)
     assert_equal 3, playlist.tracks.count
   end
 
   test "playlist can't save without user" do
-    playlist = Playlist.new
+    playlist = Playlist.new()
     assert_not playlist.save
   end
 end
