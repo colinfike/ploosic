aa2655d01313c07ffa4fdd4a7d41fa316848c3a3
Colin Fike <colin.fike@gmail.com>
2017-03-05 21:22:11 -0500

    Get this proof of concept done with the players

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def proof_of_concept
+    @url = "https://soundcloud.com/scottishfoldlp/blinded-by-light-final-fantasy"
+  end
 end
