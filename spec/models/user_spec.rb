591a04f49daadca13e62dc346814f0bb726f0876
Colin Fike <colin.fike@gmail.com>
2017-03-16 23:10:58 -0400

    Updated user spec with shoulda gem

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe User, 'user attribute tests' do
-  it 'cannot save without a username' do
-    user = build(:user, username: nil)
-    result = user.save
-    expect(result).to be false
-  end
+RSpec.describe User, "validations" do
+  it { is_expected.to validate_presence_of(:username) }
 end
