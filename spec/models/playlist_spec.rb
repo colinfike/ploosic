ba203f9487e996a5c62d88fca12376526c8fcb0c
Colin Fike <colin.fike@gmail.com>
2017-03-16 23:06:18 -0400

    Update playlist spec with shoulda gem

--- spec/models/playlist_spec.rb
+++ spec/models/playlist_spec.rb
@@ -1,12 +1,9 @@
 require 'rails_helper'
+RSpec.describe Playlist, "validations" do
+  it { is_expected.to validate_presence_of(:user) }
+end
 
-RSpec.describe User, 'playlist attribute testing' do
-  it "playlist can't save without a user" do
-    playlist = build(:playlist, user: nil)
-    result = playlist.save
-    expect(result).to be false
-  end
-
+RSpec.describe User, 'obsolete association tests' do
   it "playlist can have multiple tracks" do
     playlist = build(:playlist, :has_tracks)
     expect(playlist.tracks.count).to eq(3)
