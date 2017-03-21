1487492c8ba6444c62ff961b3c3c3af61a51948e
Colin Fike <colin.fike@gmail.com>
2017-03-21 01:28:15 -0400

    Figured out a temporary solution for the index page/new/old user flow until I can develop this a bit further. Broke a few tests to be updated tomorrow

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -2,8 +2,18 @@ class PlaylistsController < ApplicationController
   before_action :get_playlist, only: [:show]
   # before_action :playlist_access, only: [:show]
 
+  # This is a little hacky for now. If you hit the index and have a user account
+  # it redirects you to your playlist, otherwise it sends you to the sample
+  # "communal" playlist until I can flesh things out a bit more.
   def index
-    # redirect_to '/playlists/1'
+    if session[:user_id]
+      user = User.find_by(id: session[:user_id])
+      redirect_to playlist_path(user.playlist)
+      # FIXME: Handle the session being out of date or something. I.e the db has
+      # been wiped and the account no longer exists
+    else
+      redirect_to '/playlists/1'
+    end
   end
 
   def show
